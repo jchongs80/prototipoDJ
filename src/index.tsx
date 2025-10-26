34b53336cd2e77966b009e147c8455f6eedad09f
jchongs80 <jchongs80@gmail.com>
2025-10-26 15:52:10 -0500

    Actualizar

--- src/index.tsx
+++ src/index.tsx
@@ -31,7 +31,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-        <App />
+        <RootContainer />
     </ThemeProvider>
   </React.StrictMode>
 );
